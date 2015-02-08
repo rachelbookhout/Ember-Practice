7d5e66d19e614979699246320946bca1cb4038c7
rachelbookhout <rbookhou@gmail.com>
2015-02-08 12:24:27 -0500

    migrated and added model

---
+++ backend/db/migrate/20150208172127_create_projects.rb
@@ -0,0 +1,11 @@
+class CreateProjects < ActiveRecord::Migration
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps null:false
+    end
+    add_index :projects, :name, unique: true
+  end
+end
