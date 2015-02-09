460cc88d24263f2173719f693d6ad857cec72352
rachelbookhout <rbookhou@gmail.com>
2015-02-09 08:10:07 -0500

    added show method

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   namespace :api do
     namespace :v1 do
-      resources :projects, only: [:index]
+      resources :projects, only: [:index, :show]
     end
   end
 end
