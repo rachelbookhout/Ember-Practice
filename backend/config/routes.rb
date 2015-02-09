458cb38ad7d5a84ee1814e72051ff4590bbad72c
rachelbookhout <rbookhou@gmail.com>
2015-02-08 20:47:29 -0500

    added controller

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  namespace :api do
+    namespace :v1 do
+      resources :projects, only: [:index]
+    end
+  end
+end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
